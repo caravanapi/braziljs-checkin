b63426416814890d4c1570e521db81d0715278fe
Cairo Noleto <caironoleto@gmail.com>
2011-05-14 17:01:03 -0300

    Time.now!

--- models/keynote.rb
+++ models/keynote.rb
@@ -2,5 +2,5 @@ class Keynote < ActiveRecord::Base
   validates :title, :presence => true 
   validates :author, :presence => true
 
-  scope :current, lambda { where("start_at >= ?",  Time.zone.now).order(:start_at).limit(1) }
+  scope :current, lambda { where("start_at >= ?",  Time.now).order(:start_at).limit(1) }
 end
